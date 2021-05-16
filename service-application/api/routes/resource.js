fa7dcff77ff33c248383569c67ddc59c736bc7ce
Ywoosang <opellong13@gmail.com>
2021-05-17 06:47:28 +0900

    Implement folder router

--- service-application/api/routes/resource.js
+++ service-application/api/routes/resource.js
@@ -11,15 +11,44 @@ router.post('/all',async (req,res,next)=>{
     try{
         const files = await File.findAll({
             where : {
-                ownerId
-        }});
-        const folders = await Folder.findAll({
-            where : {
-                ownerId
+                ownerId,
+                folderId: null
             }
         });
-        console.log(files,folders); 
+        const folders = await Folder.findAll({
+            where : {
+                ownerId,
+                hierarchy:1
+            }
+        });
         res.json({files,folders})
+    }catch (err){
+        console.log(err);
+    }
+});
+
+
+router.post('/folder/:id',async (req,res,next)=>{
+    const folderId = req.params.id;
+    console.log('실행됌')
+    try{
+        const crrFolder = await Folder.findOne({
+            where : {
+                id:folderId 
+            }
+        })
+        const files = await File.findAll({
+            where : {
+                folderId,
+            }
+        });
+        const folders = await Folder.findAll({
+            where :{
+                hierarchy : crrFolder.hierarchy + 1,
+                codeNumber : crrFolder.codeNumber,
+            }
+        })
+        res.json({files,folders});
     }catch (err){
         console.log(err);
     }
