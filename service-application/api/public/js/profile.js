20984b5fc65ff3dfa4f5ebf5effe29d2b25ebb47
chidoll96 <chidoll96@naver.com>
2021-06-06 03:28:18 +0900

    Fix Profile page CSS design #10

--- service-application/api/public/js/profile.js
+++ service-application/api/public/js/profile.js
@@ -5,12 +5,21 @@ const open2 = () => {
     document.querySelector(".modal2").classList.remove("hidden2");
 }
 
+const open3 = () => {
+    document.querySelector(".modal3").classList.remove("hidden3");
+}
+
 const close = () => {
     document.querySelector(".modal").classList.add("hidden");
 }
 const close2 = () => {
     document.querySelector(".modal2").classList.add("hidden2");
 }
+const close3 = () => {
+    document.querySelector(".modal3").classList.add("hidden3");
+}
+
+
 document.querySelector(".openBtn").addEventListener("click", open);
 document.querySelector(".closeBtn").addEventListener("click", close);
 document.querySelector(".changebtn").addEventListener("click", close);
@@ -20,6 +29,11 @@ document.querySelector(".openBtn2").addEventListener("click", open2);
 document.querySelector(".closeBtn2").addEventListener("click", close2);
 document.querySelector(".changebtn2").addEventListener("click", close2);
 document.querySelector(".bg2").addEventListener("click", close2);
+
+document.querySelector(".img-preview").addEventListener("click", open3);
+document.querySelector(".img-submit").addEventListener("click", close3);
+document.querySelector(".bg3").addEventListener("click", close3);
+document.querySelector(".closeBtn3").addEventListener("click", close3);
 
 const fileCheck = document.querySelector('.user-img');
 console.log(fileCheck);
