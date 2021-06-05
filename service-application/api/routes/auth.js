d82432b7f7231727c786b75e9b6b755d2827260a
Ywoosang <opellong13@gmail.com>
2021-06-06 00:39:16 +0900

    Implement profile management api

--- service-application/api/routes/auth.js
+++ service-application/api/routes/auth.js
@@ -3,12 +3,12 @@ const router = express.Router();
 const passport = require('passport')
 const path = require('path')
 const bcrypt = require('bcrypt');
-const { User }= require('../models');
-const { Op } = require('sequelize'); 
+const { User } = require('../models');
+const { Op } = require('sequelize');
 // 로컬 로그인
 router.route('/signup')
     .get((req, res) => {
-        return  res.sendFile(path.join(__dirname,'../views/signup.html'))  
+        return res.sendFile(path.join(__dirname, '../views/signup.html'))
     })
     .post(async (req, res, next) => {
         const name = req.body.name;
@@ -23,7 +23,7 @@ router.route('/signup')
             }
             const hash = await bcrypt.hash(password, 12);
             await User.create({
-                nickname:name,
+                nickname: name,
                 email,
                 password: hash
             })
@@ -36,7 +36,7 @@ router.route('/signup')
 
 router.route('/login')
     .get((req, res) => {
-        return  res.sendFile(path.join(__dirname,'../views/login.html'))  
+        return res.sendFile(path.join(__dirname, '../views/login.html'))
     })
     .post((req, res, next) => {
         passport.authenticate('local', (authError, user, info) => {
@@ -58,57 +58,97 @@ router.route('/login')
     });
 
 router.route('/profile')
-.get(async(req,res,next)=>{
+    .get(async (req, res, next) => {
+        try {
+            const id = req.user.id;
+            const user = await User.findOne({
+                where: {
+                    id
+                }
+            })
+            res.render('profile', { user });
+        } catch (err) {
+            next(err);
+        }
+
+    })
+router.post('/profile/id', async(req,res,next) => {
     try{
+    const id = req.user.id;
+    const nickId = req.body.nickId;
+    if(!nickId) return res.redirect('/error?message="값을 입력해주세요"'); 
+    if (nickId.length > 10) {
+        // 에러처리 할것
+        return res.redirect('/error?message="아이디 값이 너무 깁니다"');
+    }
+    const pattern = /^[A-Za-z0-9+]*$/;
+    if (!pattern.test(nickId)) {
+        return res.redirect('/error?message="아이디는 영문과 숫자로 이루어져 있어야 합니다."');
+    }
+    const isnickId = await User.findOne({
+        where: {
+            nickId,
+            id: {
+                [Op.ne]: id
+            }
+        }
+    })
+    if (isnickId) {
+        return res.redirect('/error?message="이미 존재하는 아이디 입니다."');
+    }
+    await User.update({
+        nickId,
+        updatedAt: new Date(),
+    }, {
+        where: {
+            id
+        }
+    });
+    res.redirect('/auth/profile');
+} catch (err) {
+    next(err);
+}
+})
+router.post('/profile/name', async(req,res,next) => {
+
+    try {
         const id = req.user.id;
-        const user = await User.findOne({
-            where : {
+        const nickname = req.body.name;
+        if (nickname.length > 10) {
+            // 에러처리 할것
+            return res.redirect('/error?message="이름 값이 너무 깁니다"');
+        }
+        await User.update({
+            nickname,
+            updatedAt: new Date(),
+        }, {
+            where: {
                 id
             }
-        })
-        res.render('profile',{user});
-    }catch(err){
-        next(err); 
-    }
-    
-}) 
-.post(async(req,res,next)=>{
-    try{   
-        const name = req.body.name;
-        const nickId = req.body.nickId;
-        const id = req.user.id;
-        if(authId.length>10 || name.length > 10){
-            // 에러처리 할것
-            return res.redirect('/error?message="아이디 또는 이름 값이 너무 깁니다"');
-        }
-        const pattern =  /^[A-Za-z0-9+]*$/; 
-        if(!pattern.test(authId)){
-            return res.redirect('/error?message="아이디는 영문과 숫자로 이루어져 있어야 합니다."');
-        }   
-        const isAuthId = await User.findOne({
-            where :{
-                nickId,
-                id : {
-                    [Op.ne] : id
-                }  
-            }
-        })
-        if(isAuthId){
-            return res.redirect('/error?message="이미 존재하는 아이디 입니다."');
-        }
-        // 
-        await User.update({
-            nickname:name,
-            nickId,
-            updatedAt: new Date(),
-        },  {where: {
-            id
-        }});
-        res.redirect('/auth/profile'); 
-    }catch(err){
+        });
+        res.redirect('/auth/profile');
+    } catch (err) {
         next(err);
     }
+})
+const { upload } = require('../index'); 
 
+router.post('/profile/image', upload.single('file'),async (req, res, next) => {
+    try {
+        const id = req.user.id;
+        const img = req.file.location;
+        await User.update({
+            img,
+            updatedAt: new Date(),
+        }, {
+            where: {
+                id
+            }
+        });
+        res.redirect('/auth/profile');
+    } catch (err) {
+        next(err);
+    }
 })
 
 // 깃허브 로그인
@@ -122,18 +162,18 @@ router.get('/github/callback',
 
 // 카카오 로그인
 router.get('/kakao', passport.authenticate('kakao'));
-router.get('/kakao/callback', 
+router.get('/kakao/callback',
     passport.authenticate('kakao', {
-    failureRedirect: '/',
-}), (req, res) => {
-    res.redirect('/');
-});
+        failureRedirect: '/',
+    }), (req, res) => {
+        res.redirect('/');
+    });
 
 // 로그아웃
 router.get('/logout', (req, res) => {
     req.logout();
-    req.session.destroy(()=>{
-       req.session; 
+    req.session.destroy(() => {
+        req.session;
     });
     res.redirect('/');
 });
